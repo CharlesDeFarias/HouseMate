cd1728154992215def32b116097efb1ca13d9458
DeFariasConsulting <defariasconsulting@gmail.com>
2019-04-21 16:31:49 -0400

    Added add house system

--- app/models/user.js
+++ app/models/user.js
@@ -5,49 +5,47 @@ var bcrypt   = require('bcrypt-nodejs');
 // define the schema for our user model
 var userSchema = mongoose.Schema({
 
-    local            : {
-        email        : String,
-        password     : String,
-        name         : String
-        // house        : String,
-        // paypal       : String,
-        // cashapp      : String,
-        // profile      : String,
-        // bills        : [],
-        // transactions : [],
-        // notes        : [],
-        // photo        : String,
-        // documents    : []
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        name         : String,
-        email        : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    }
+  local            : {
+    email        : String,
+    password     : String,
+    house        : String,
+    photo        : String
+    // name         : String,
+    // paypal       : String,
+    // cashapp      : String,
+    // profile      : String,
+    // notes        : [],
+    // documents    : []
+  },
+  facebook         : {
+    id           : String,
+    token        : String,
+    name         : String,
+    email        : String
+  },
+  twitter          : {
+    id           : String,
+    token        : String,
+    displayName  : String,
+    username     : String
+  },
+  google           : {
+    id           : String,
+    token        : String,
+    email        : String,
+    name         : String
+  }
 
 });
 
 // generating a hash
 userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+  return bcrypt.compareSync(password, this.local.password);
 };
 
 // create the model for users and expose it to our app
--- app/routes.js
+++ app/routes.js
@@ -1,141 +1,204 @@
-  module.exports = function(app, passport, db, multer, ObjectId) {
+module.exports = function(app, passport, db, multer, ObjectId) {
 
-    // normal routes ===============================================================
+  // normal routes ===============================================================
 
-    // show the home page (will also have our login links)
-    app.get('/', function(req, res) {
-      res.render('landing.ejs');
-    });
+  // show the home page (will also have our login links)
+  app.get('/', function(req, res) {
+    res.render('landing.ejs');
+  });
 
-    ///Bill and New Bill pages
+  ///Bill and New Bill pages
 
-    //all bills page
-    app.get('/bills', isLoggedIn, function(req, res) {
-      //Currently finding all bills. Want to make find all bills in the house the user viewing is in.
-      db.collection('bills').find().toArray((err, bills) => {
-        if (err) return console.log(err)
-        // console.log(`This is the bills get routes.js :`, bills)
-        res.render('bills.ejs', {
-          bills: bills,
-          user: req.user
-        })
+  //all bills page
+  app.get('/bills', isLoggedIn, function(req, res) {
+    //Currently finding all bills. Want to make find all bills in the house the user viewing is in.
+    db.collection('bills').find({'houseId': ObjectId(req.user.local.house)}).toArray((err, bills) => {
+      if (err) return console.log(err)
+      // console.log(`This is the bills get routes.js :`, bills)
+      res.render('bills.ejs', {
+        bills: bills,
+        user: req.user
       })
     })
+  })
 
-    app.get('/bill', isLoggedIn, function(req, res) {
-      let queryParam = ObjectId(req.query.billId)
-      console.log("this is the queryParam variable:",queryParam)
-      db.collection('bills').find({_id: queryParam}).toArray((err, bill) => {
-        if (err) return console.log(err)
-        console.log(`This is the bill get routes.js :`, bill)
-        res.render('bill.ejs', {
-          bill: bill,
-          user: req.user
-        })
+  app.get('/bill', isLoggedIn, function(req, res) {
+    let queryParam = ObjectId(req.query.billId)
+    console.log("this is the queryParam variable:",queryParam)
+    db.collection('bills').find({_id: queryParam}).toArray((err, bill) => {
+      if (err) return console.log(err)
+      console.log(`This is the bill get routes.js :`, bill)
+      res.render('bill.ejs', {
+        bill: bill,
+        user: req.user
       })
     })
+  })
 
 
-    //Making a payment from bill page
-    app.put('/makePayment', (req, res) => {
-      console.log("make payment put route with billId", req.body.billId)
-      db.collection('bills')
-      .findOneAndUpdate({_id: ObjectId(req.body.billId)}, {
+  //Making a payment from bill page
+  app.put('/makePayment', (req, res) => {
+    console.log("make payment put route with billId", req.body.billId)
+    db.collection('bills')
+    .findOneAndUpdate({_id: ObjectId(req.body.billId)}, {
+      $set: {
+        amountLeft: parseFloat(req.body.amountLeft) - (parseFloat(req.body.amountOriginal)/parseFloat(req.body.numPeople))
+      },
+      $push: {
+        peoplePaid: req.user.local.email
+      }
+    }, {
+      sort: {_id: -1},
+      upsert: false
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    })
+  })
+
+  //Creating a new bill
+  app.get('/newBill', isLoggedIn, function(req, res) {
+    res.render('newBill.ejs', {
+      user: req.user
+    })
+  })
+
+  app.post('/newBill', isLoggedIn, (req, res) => {
+    console.log("This is /newbill post req.user", req.user)
+    db.collection('bills').save({name: req.body.billName, numPeople: req.body.numPeople, billDesc: req.body.billDesc,  amountOriginal: req.body.billAmount, amountLeft: req.body.billAmount, houseId: ObjectId(req.user.local.house), peoplePaid: [] }, (err, result) => {
+      if (err) return console.log(err)
+      console.log('saved to database')
+      res.redirect('/bills')
+    })
+  })
+
+  //page seen when logged in
+  app.get('/checkMember', isLoggedIn, function(req, res) {
+    console.log("this is the /checkMember filter", req.user.local.house)
+    if(req.user.local.house){
+      res.redirect('/bills')
+    }else{
+      res.redirect('/moveIn')
+    }
+  })
+
+  //page to either create new house or join existing one
+  app.get('/moveIn', isLoggedIn, function(req, res) {
+    res.render('moveIn.ejs', {
+      user: req.user
+    })
+  })
+
+  app.post('/createHouse', (req, res) => {
+    db.collection('houses').insert({
+      houseName: req.body.houseName, address: req.body.address, wifi: {wifiName: req.body.wifiName,  wifiPass: req.body.wifiPass}
+    }, (err, result) => {
+      console.log("this is the /createHouse route", result)
+      if (err) return console.log(err)
+      console.log("this is the insertedIDs variable from /createHouse route", result.insertedIds["0"])
+      db.collection('users').findOneAndUpdate({
+        'local.email': req.user.local.email
+      }, {
         $set: {
-          amountLeft: parseFloat(req.body.amountLeft) - (parseFloat(req.body.amountOriginal)/parseFloat(req.body.numPeople))
-        },
-        $push: {
-          peoplePaid: req.user.local.email
+          'local.house': result.insertedIds["0"]
+          //can add a .toString() after the bracket if the objectId aspect's giving trouble. Change it to a string and use objectId function to search with it
         }
       }, {
         sort: {_id: -1},
         upsert: false
       }, (err, result) => {
-        if (err) return res.send(err)
-        res.send(result)
-      })
-    })
-
-    //Creating a new bill
-    app.get('/newBill', isLoggedIn, function(req, res) {
-        res.render('newBill.ejs', {
-          user: req.user
-        })
-      })
-
-    app.post('/newBill', (req, res) => {
-      db.collection('bills').save({name: req.body.billName, numPeople: req.body.numPeople, billDesc: req.body.billDesc,  amountOriginal: req.body.billAmount, amountLeft: req.body.billAmount, houseId: null, peoplePaid: [] }, (err, result) => {
-        if (err) return console.log(err)
-        console.log('saved to database')
+        if (err){
+          console.log("this is the /createhouse user update error", err)
+          return res.send(err)
+        }
         res.redirect('/bills')
       })
     })
+  })
 
-    // PROFILE SECTION =========================
+  app.post('/joinHouse', (req, res) => {
+    //route for when join house form is submitted in moveIn.ejs
+    db.collection('users')
+    .findOneAndUpdate({
+      "local.email": req.user.local.email
+    }, {
+      $set:{
+        "local.house": ObjectId(req.body.houseId)
+      }
+    }, {
+      sort: {_id: -1},
+      upsert: false
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.redirect('/bills')
+    })
+  })
 
-    //HOUSE PROFILE PAGE
+  // PROFILE SECTION =========================
+
+  //HOUSE PROFILE PAGE
 
 
-    //PUBLIC PROFILE PAGE
+  //PUBLIC PROFILE PAGE
 
 
-    //Main portal after logging in
+  //Main portal after logging in
 
-    // LOGOUT ==============================
-    app.get('/logout', function(req, res) {
-      req.logout();
-      res.redirect('/');
-    });
-
-    // =============================================================================
-    // AUTHENTICATE (FIRST LOGIN) ==================================================
-    // =============================================================================
-
-    // locally --------------------------------
-    // LOGIN ===============================
-    // show the login form
-    app.get('/login', function(req, res) {
-      res.render('login.ejs', { message: req.flash('loginMessage') });
-    });
-
-    // process the login form
-    app.post('/login', passport.authenticate('local-login', {
-      successRedirect : '/bills', // redirect to the secure profile section
-      failureRedirect : '/', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-    }));
-
-    // SIGNUP =================================
-    // process the signup form
-    app.post('/signup', passport.authenticate('local-signup', {
-      successRedirect : '/bills', // redirect to the secure profile section
-      failureRedirect : '/', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-    }));
-
-    // =============================================================================
-    // UNLINK ACCOUNTS =============================================================
-    // =============================================================================
-    // used to unlink accounts. for social accounts, just remove the token
-    // for local account, remove email and password
-    // user account will stay active in case they want to reconnect in the future
-
-    // local -----------------------------------
-    app.get('/unlink/local', isLoggedIn, function(req, res) {
-      var user            = req.user;
-      user.local.email    = undefined;
-      user.local.password = undefined;
-      user.save(function(err) {
-        res.redirect('/profile');
-      });
-    });
-
-  };
-
-  // route middleware to ensure user is logged in
-  function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated())
-    return next();
-
+  // LOGOUT ==============================
+  app.get('/logout', function(req, res) {
+    req.logout();
     res.redirect('/');
-  }
+  });
+
+  // =============================================================================
+  // AUTHENTICATE (FIRST LOGIN) ==================================================
+  // =============================================================================
+
+  // locally --------------------------------
+  // LOGIN ===============================
+  // show the login form
+  app.get('/login', function(req, res) {
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+  });
+
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/checkMember', // redirect to the secure profile section
+    failureRedirect : '/', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+
+  // SIGNUP =================================
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/moveIn', // redirect to the secure profile section
+    failureRedirect : '/', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
+
+  // local -----------------------------------
+  app.get('/unlink/local', isLoggedIn, function(req, res) {
+    var user            = req.user;
+    user.local.email    = undefined;
+    user.local.password = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
+    });
+  });
+
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+  return next();
+
+  res.redirect('/');
+}
